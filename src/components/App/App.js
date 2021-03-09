a9e4fa3b68a48148410946623cb46e04a80a1da5
betsy <betsy.groton@earlybirdeducation.com>
2021-03-08 19:54:23 -0500

    feat: re reouting and loading everything on app

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,7 +1,25 @@
 import React from 'react';
+import AboutMe from '../AboutMe';
+import Articles from '../Articles';
+import ContactMe from '../ContactMe';
+import Landing from '../Landing';
+import Navigation from '../Navigation';
+import Projects from '../Projects';
+import TechStack from '../TechStack';
 
 const App = () => {
-  return <div>App</div>;
+  return (
+    <div>
+      <h1>App</h1>
+      <Navigation />
+      <Landing />
+      <AboutMe />
+      <TechStack />
+      <Projects />
+      <Articles />
+      <ContactMe />
+    </div>
+  );
 };
 
 export default App;
---
+++ src/constants/routes.js
@@ -0,0 +1,8 @@
+// routes
+
+export const LANDING = '/landing';
+export const ABOUTME = '/about-me';
+export const TECHSTACK = '/tech-stack';
+export const PROJECTS = '/projects';
+export const ARTICLES = '/articles';
+export const CONTACTME = '/contact-me';
